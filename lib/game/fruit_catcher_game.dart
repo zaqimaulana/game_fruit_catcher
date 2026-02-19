52384f5f29cb96257786b4d028b463e6d50692b3
zaqimaulana <zaqimaulana72@gmail.com>
2026-02-19 17:20:15 +0700

    update baru fruit_catcher_game.dart

--- lib/game/fruit_catcher_game.dart
+++ lib/game/fruit_catcher_game.dart
@@ -1,28 +1,102 @@
+import 'dart:math';
+import 'package:flame/camera.dart';
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+
+import 'components/basket.dart';
+import 'components/fruit.dart';
 import 'managers/audio_manager.dart';
 
-class FruitCatcherGame extends FlameGame {
+class FruitCatcherGame extends FlameGame
+    with PanDetector, HasCollisionDetection {
 
-  @override
-  Color backgroundColor() => const Color(0xFF87CEEB);
+  late Basket basket;
+
+  final Random random = Random();
+
+  double fruitSpawnTimer = 0;
+  final double fruitSpawnInterval = 1.5;
 
   final ValueNotifier<int> scoreNotifier = ValueNotifier<int>(0);
+
+  int _score = 0;
+
+  int get score => _score;
+
+  set score(int value) {
+    _score = value;
+    scoreNotifier.value = value;
+  }
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
 
+    // Set viewport resolution
+    camera.viewport = FixedResolutionViewport(
+      resolution: Vector2(400, 800),
+    );
+
+    // Add basket
+    basket = Basket();
+    await add(basket);
+
+    // Play background music
     AudioManager().playBackgroundMusic();
   }
 
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    // Spawn fruit setiap interval
+    fruitSpawnTimer += dt;
+
+    if (fruitSpawnTimer >= fruitSpawnInterval) {
+      spawnFruit();
+      fruitSpawnTimer = 0;
+    }
+  }
+
+  void spawnFruit() {
+    final x = random.nextDouble() * size.x;
+
+    final fruit = Fruit(
+      position: Vector2(x, -50),
+    );
+
+    add(fruit);
+  }
+
+  @override
+  void onPanUpdate(DragUpdateInfo info) {
+    basket.position.x += info.delta.global.x;
+
+    // Batas kiri & kanan layar
+    basket.position.x = basket.position.x.clamp(
+      basket.size.x / 2,
+      size.x - basket.size.x / 2,
+    );
+  }
+
   void incrementScore() {
-    scoreNotifier.value++;
+    score++;
+    AudioManager().playSfx('collect.mp3');
+  }
+
+  void gameOver() {
+    AudioManager().playSfx('explosion.mp3');
+    pauseEngine();
   }
 
   @override
   void onRemove() {
-    scoreNotifier.dispose();
+    AudioManager().stopBackgroundMusic();
     super.onRemove();
   }
+
+  @override
+  Color backgroundColor() => const Color(0xFF87CEEB);
 }
